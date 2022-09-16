3850d58ea70d2bea1ac3425332b48033db75ff2c
Abhi Aiyer <abhiaiyer91@gmail.com>
2022-09-16 16:33:50 -0700

    nvm

--- frameworks/svelte/src/lib/contentful.js
+++
@@ -1,4 +0,0 @@
-import * as contentful from 'contentful';
-import { contentful as contentfulAuth } from './variables'
-
-export const client = contentful.createClient(contentfulAuth);
--- frameworks/svelte/src/lib/variables.js
+++
@@ -1,5 +0,0 @@
-export const contentful = {
-  space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN
-}
-

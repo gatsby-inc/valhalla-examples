981d4510df02a4f692260420125f21d5e6883367
Dustin Schau <dustinschau@gmail.com>
2022-10-20 16:16:51 -0700

    chore: refactor astro and get homepage working

---
+++ frameworks/astro/src/lib/client.ts
@@ -0,0 +1,10 @@
+import fetch from "node-fetch";
+import { createClient } from "@urql/core";
+
+const API_URL = import.meta.env.PUBLIC_VALHALLA_API_URL;
+
+export const client = createClient({
+  url: API_URL,
+  fetch,
+});
+

b17d3f61908b246a237f7e791f2512d6ed41e004
Florian Kissling <florian.kissling@gmail.com>
2022-10-22 03:42:24 +0200

    chore(nextjs): rm obsolete CSS and imports

--- frameworks/nextjs/pages/_app.js
+++ frameworks/nextjs/pages/_app.js
@@ -1,9 +1,3 @@
-import '../styles/globals.css'
-import "../public/assets/fonts/futura-pt/Webfonts/futurapt_book_macroman/stylesheet.css";
-import "../public/assets/fonts/futura-pt/Webfonts/futurapt_demi_macroman/stylesheet.css";
-import "../public/assets/fonts/futura-pt/Webfonts/futurapt_bold/stylesheet.css";
-
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }

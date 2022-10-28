be177a19db175d001c82f27ec1da4144f4a3bf17
Florian Kissling <florian.kissling@gmail.com>
2022-10-28 04:51:04 +0200

    Animals is a G

--- frameworks/nextjs/pages/components/Animals.tsx
+++ frameworks/nextjs/pages/components/Animals.tsx
@@ -1,172 +1,17 @@
 import React, { useState } from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
-import styled, { css } from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
+import clsx from "clsx";
 
-const PageTitle = styled.h1`
-  color: var(--color-fg);
-  margin: 0 0 var(--size-4);
-  font-size: var(--font-size-6);
-`;
-
-const Description = styled.div`
-  color: var(--color-fg);
-  font-size: var(--font-size-3);
-  line-height: var(--lineheight-4);
-`;
-
-export const AnimalList = styled.section`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin: var(--size-5) 0;
-  padding-bottom: var(--size-5);
-  gap: var(--size-6);
-  justify-content: space-between;
-`;
-
-const ImageWrapper = styled.div`
-  background: var(--color-bg-calm);
-  border-radius: var(--radius-5);
-  overflow: hidden;
-  flex-shrink: 0;
-`;
-
-const AnimalName = styled.span`
-  font-size: var(--font-size-2);
-  font-weight: var(--font-weight-7);
-  line-height: var(--lineheight-1);
-  color: var(--color-fg);
-  margin: 0;
-  display: inline-block;
-`;
-
-const ViewDetails = styled.a`
-  display: inline-flex;
-  align-items: baseline;
-  text-decoration: none;
-  text-underline-offset: 2px;
-
-  :before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-  }
-`;
-
-const ViewDetailsIcon = styled.span`
-  text-decoration: none;
-  display: inline-block;
-  width: var(--size-3);
-  height: var(--size-3);
-  color: var(--color-fg-calm);
-  transition-property: all;
-  transition-duration: 0.125s;
-  transition-timing-function: ease-in-out;
-
-  > svg {
-    vertical-align: middle;
-  }
-`;
-
-const Card = styled.div`
-  align-self: flex-start;
-  box-sizing: content-box;
-  color: var(--color-fg-calm);
-  line-height: var(--line-height-3);
-  position: relative;
-  display: flex;
-  flex-direction: ${(props) => (props.disableDetails ? "row" : "column")};
-  font-size: var(--font-size-1);
-  gap: ${(props) =>
-    props.disableDetails
-      ? "var(--size-6)"
-      : "calc(var(--size-2) + var(--size-2))"};
-  margin-top: ${(props) =>
-    props.disableDetails ? "var(--size-7)" : "var(--size-0)"};
-  width: ${(props) => (props.disableDetails ? "auto" : "min-content")};
-
-  ${ImageWrapper} {
-    box-shadow: var(--shadow-elevation-medium);
-    transition-property: all;
-    transition-duration: 0.125s;
-    transition-timing-function: ease-in-out;
-    // because Next's <Image> is rendered as inline-block
-    // and https://github.com/vercel/next.js/issues/18915 is a rabbit hole
-    // i don't have time for
-    height: ${(props) => (props.disableDetails ? `400px` : `200px`)};
-  }
-
-  ${(props) =>
-    !props.disableDetails
-      ? css`
-          &:hover ${ImageWrapper} {
-            box-shadow: var(--shadow-elevation-high);
-            transform: scale(1.025);
-          }
-
-          &:hover ${ViewDetails} {
-            color: var(--color-accent);
-          }
-
-          &:hover ${AnimalName} {
-            color: var(--color-accent);
-          }
-
-          &:hover ${ViewDetailsIcon} {
-            color: var(--color-accent);
-            left: 5px;
-            transition-property: all;
-            transition-duration: 0.125s;
-            transition-timing-function: ease-in-out;
-          }
-        `
-      : css`
-          ${ImageWrapper} {
-            box-shadow: var(--shadow-elevation-high);
-          }
-        `}
-
-  @media only screen and (max-width: 500px) {
-    width: 100%;
-    flex: auto;
-    height: auto;
-  }
-`;
-
-const AnimalImage = styled.img`
-  width: 200px;
-  margin: 0 auto;
-  object-fit: cover;
-  height: 200px;
-  display: block;
-
-  @media only screen and (max-width: 500px) {
-    width: 100%;
-    height: 200px;
-  }
-`;
-
-const AnimalType = styled.span`
-  font-size: var(--font-size-0);
-  line-height: var(--lineheight-0);
-  color: var(--color-fg-calm);
-  padding-top: 6px;
-  text-transform: capitalize;
-  float: right;
-`;
+import styles from "../../../styles/animals.module.css"
 
 const DetailLink = ({ children, href }) => (
   <Link passHref href={href}>
-    <ViewDetails>
-      <AnimalName>
+    <a className={styles.viewDetailsLink}>
+      <h2 className={styles.animalName}>
         {children}
-        <ViewDetailsIcon>
+        <span className={styles.viewDetailsIcon}>
           <svg
             width="1em"
             height="1em"
@@ -182,9 +27,9 @@ const DetailLink = ({ children, href }) => (
               strokeLinejoin="round"
             />
           </svg>
-        </ViewDetailsIcon>
-      </AnimalName>
-    </ViewDetails>
+        </span>
+      </h2>
+    </a>
   </Link>
 );
 
@@ -193,8 +38,8 @@ export function AnimalDisplay({ animal, type, disableDetails = false }) {
   const [name, setName] = useState(animal?.name);
 
   return (
-    <Card disableDetails={disableDetails}>
-      <ImageWrapper>
+    <div className={clsx(styles.card, !disableDetails && styles.cardDetails)}>
+      <div className={styles.imageWrapper}>
         {animalState?.image?.gatsbyImageData ? (
           <GatsbyImage
             image={animalState?.image?.gatsbyImageData}
@@ -214,21 +59,21 @@ export function AnimalDisplay({ animal, type, disableDetails = false }) {
             layout="fixed"
           />
         )}
-      </ImageWrapper>
+      </div>
       <div>
         {disableDetails ? (
-          <PageTitle>{animalState?.name || `Good Boy`}</PageTitle>
-        ) : (
+          <h1 className={styles.pageTitle}>{animalState?.name || `Good Boy`}</h1>
+          ) : (
           <DetailLink href={`/${type}/${animalState?.id}/`}>
             {animalState?.name || `Good Boy`}
           </DetailLink>
         )}
-        {type && <AnimalType>{type}</AnimalType>}
+        {type && <span className={styles.animalType}>{type}</span>}
         {disableDetails && animalState?.about?.about && (
-          <Description>{animalState?.about?.about}</Description>
+          <div className={styles.description}>{animalState?.about?.about}</div>
         )}
       </div>
-    </Card>
+    </div>
   );
 }
 
@@ -240,10 +85,10 @@ export function Animal({ animal }) {
 
 export function Animals({ data }) {
   return (
-    <AnimalList>
+    <section className={styles.animalList}>
       {data?.map((animal, i) => {
         return <Animal key={`${animal.name}${i}`} animal={animal} />;
       })}
-    </AnimalList>
+    </section>
   );
 }

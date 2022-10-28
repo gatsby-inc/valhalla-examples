ec8121fcaeb67fd64c8bcaba2d71fe629e571063
Dustin Schau <dustinschau@gmail.com>
2022-10-28 11:57:38 -0700

    chore: start using app directory so we can use server components

--- frameworks/nextjs/next.config.js
+++ frameworks/nextjs/next.config.js
@@ -8,6 +8,9 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+  experimental: {
+    appDir: true
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,

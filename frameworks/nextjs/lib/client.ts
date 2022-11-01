1d64d93f7a10bd12248a4d5cf7bc639de996d416
Dustin Schau <dustinschau@gmail.com>
2022-11-01 16:06:22 -0700

    chore: remove GATSBY_ from API URL

--- frameworks/nextjs/lib/client.ts
+++ frameworks/nextjs/lib/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@urql/core";
 
-const API_URL = process.env.GATSBY_VALHALLA_API_URL;
+const API_URL = process.env.VALHALLA_API_URL;
 
 export const client = createClient({
   url: API_URL,

ba94a5979caf3ad53ea316008bab5ba617beac45
Dustin Schau <dustinschau@gmail.com>
2022-11-01 14:11:29 -0700

    chore: remove styled components flag

--- frameworks/nextjs/next.config.js
+++ frameworks/nextjs/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
   experimental: {
     appDir: true
   },

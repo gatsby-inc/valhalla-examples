6f76b5217fa053fc13c1b7f95579244ab867c1a9
Florian Kissling <florian.kissling@gmail.com>
2022-11-03 00:42:34 +0100

    chore(gatsby): switch font to variable, mv import
    - switch to variable version of `Inter` – fixes rendering in Safari
    - mv font import to `gatsby-browser` – as recommended (https://www.gatsbyjs.com/docs/how-to/styling/using-web-fonts/#self-host-google-fonts-with-fontsource)

--- frameworks/gatsby/gatsby-browser.js
+++ frameworks/gatsby/gatsby-browser.js
@@ -2,6 +2,7 @@ const React = require('react')
 const { ApolloProvider } = require('@apollo/client')
 const { client } = require('./src/apollo')
 require('../styles/global.css')
+require('@fontsource/inter/variable-full.css')
 
 // Wraps every page in a component
 exports.wrapPageElement = ({ element, props }) => {
--- frameworks/gatsby/src/components/Layout.tsx
+++ frameworks/gatsby/src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import '@fontsource/inter'
 
 import logo from '../assets/logo.svg'
 import framework from '../assets/framework.svg'
